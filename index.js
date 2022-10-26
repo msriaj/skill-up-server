7ca4e4081277d1e6598dc285213c874a92d7099e
md-riaj11 <contact.riaj@gmail.com>
2022-10-26 17:45:29 +0600

    api created express and cors installed

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const category = require("./data/category.json");
+const courses = require("./data/courses.json");
+const port = 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(category);
+});
+
+app.get("/category/:id", (req, res) => {
+  const catID = req.params.id;
+
+  console.log(catID);
+
+  if (catID == 6) {
+    res.send(courses);
+  } else {
+    const courseByCat = courses.filter((course) => course.catID == catID);
+    res.send(courseByCat);
+  }
+});
+
+app.get("/course/:id", (req, res) => {
+  const courseID = req.params.id;
+  const courseByID = courses.find((course) => course.courseID == courseID);
+  res.send(courseByID);
+});
+
+app.listen(port, (result) => {
+  console.log("server running on ", port);
+});
